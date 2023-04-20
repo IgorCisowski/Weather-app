0639569e163bedb52f2ac1bec16b4b068fd41928
IgorCisowski <igorcisowski2098@gmail.com>
2023-04-20 16:00:03 +0200

    feat: changed fetch handlers from then to async

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import "./App.css";
+import search from "./images/search.svg";
 
 function App() {
   const [place, setPlace] = useState("London");
@@ -17,7 +18,6 @@ function App() {
         return response.json();
       })
       .then((data) => {
-        console.log(placeInfo);
         setPlaceInfo({
           name: data.name,
           icon: data.weather[0].icon,
@@ -29,8 +29,10 @@ function App() {
         setPlace("");
       });
   };
+
+  console.log(placeInfo);
   return (
-    <body
+    <section
       style={{
         backgroundImage: `url('https://source.unsplash.com/1600x900/?${placeInfo.name}')`,
       }}
@@ -51,17 +53,7 @@ function App() {
             }}
           />
           <button onClick={handleFetch}>
-            <svg
-              stroke="currentColor"
-              fill="currentColor"
-              strokeWidth="0"
-              viewBox="0 0 1024 1024"
-              height="1.5em"
-              width="1.5em"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path d="M909.6 854.5L649.9 594.8C690.2 542.7 712 479 712 412c0-80.2-31.3-155.4-87.9-212.1-56.6-56.7-132-87.9-212.1-87.9s-155.5 31.3-212.1 87.9C143.2 256.5 112 331.8 112 412c0 80.1 31.3 155.5 87.9 212.1C256.5 680.8 331.8 712 412 712c67 0 130.6-21.8 182.7-62l259.7 259.6a8.2 8.2 0 0 0 11.6 0l43.6-43.5a8.2 8.2 0 0 0 0-11.6zM570.4 570.4C528 612.7 471.8 636 412 636s-116-23.3-158.4-65.6C211.3 528 188 471.8 188 412s23.3-116.1 65.6-158.4C296 211.3 352.2 188 412 188s116.1 23.2 158.4 65.6S636 352.2 636 412s-23.3 116.1-65.6 158.4z"></path>
-            </svg>
+            <img src={search} alt="search" />
           </button>
         </div>
         <div className="weather-container">
@@ -79,7 +71,7 @@ function App() {
           <div className="wind">Wind speed: {placeInfo.wind} km/h</div>
         </div>
       </div>
-    </body>
+    </section>
   );
 }
 
